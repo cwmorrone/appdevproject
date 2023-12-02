e6205938cf9790e37c37961d9e4218d2cd841974
cwmorrone <70601877+cwmorrone@users.noreply.github.com>
2023-12-02 12:57:41 -0500

    Create AuthenticationManager.swift

---
+++ AuthenticationManager.swift
@@ -0,0 +1,16 @@
+import Foundation
+import Firebase
+
+class AuthenticationManager {
+    func signIn(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: completion)
+    }
+
+    func signUp(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: completion)
+    }
+
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+}
